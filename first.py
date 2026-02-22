b33924a71a1c5e10c44ae36a9cf6fb92b7f349c8
Johan Lidén <juanliden@gmail.com>
2026-02-22 17:58:35 +0100

    add functionality up to tutorial 11

--- first.py
+++ first.py
@@ -1,16 +1,69 @@
+from time import monotonic
+from textual import on
 from textual.app import App
+from textual.reactive import reactive
+from textual.containers import ScrollableContainer
 from textual.widgets import Button, Footer, Header, Static
 
 
 class TimeDisplay(Static):
     """custom time display"""
 
+    accumulated_time = 0
+    start_time = monotonic()
+    time_elapsed = reactive(0)
+
+    def on_mount(self):
+        self.update_timer = self.set_interval(
+            1 / 60,
+            self.update_time_elapsed,
+            pause=True,
+        )
+
+    def update_time_elapsed(self):
+        self.time_elapsed = self.accumulated_time + monotonic() - self.start_time
+
+    def watch_time_elapsed(self):
+        time = self.time_elapsed
+        time, seconds = divmod(time, 60)
+        hours, minutes = divmod(time, 60)
+        time_string = f"{hours:02.0f}:{minutes:02.0f}:{seconds:05.2f}"
+        self.update(time_string)
+
+    def start(self):
+        self.start_time = monotonic()
+        self.update_timer.resume()
+
+    def stop(self):
+        # self.time_elapsed = monotonic() - self.start_time
+        self.accumulated_time = self.time_elapsed
+        self.update_timer.pause()
+
+    def reset(self):
+        self.time_elapsed = 0
+        self.update_timer.pause()
+
 
 class Stopwatch(Static):
+    @on(Button.Pressed, "#start")
+    def start_stopwatch(self):
+        self.add_class("started")
+        self.query_one(TimeDisplay).start()
+
+    @on(Button.Pressed, "#stop")
+    def stop_stopwatch(self):
+        self.remove_class("started")
+        self.query_one(TimeDisplay).stop()
+
+    @on(Button.Pressed, "#reset")
+    def reset_stopwatch(self):
+        self.remove_class("started")
+        self.query_one(TimeDisplay).reset()
+
     def compose(self):
-        yield Button("Start", variant="success")
-        yield Button("Stop", variant="error")
-        yield Button("Reset")
+        yield Button("Start", variant="success", id="start")
+        yield Button("Stop", variant="error", id="stop")
+        yield Button("Reset", id="reset")
         yield TimeDisplay("00:00:00.00")
 
 
@@ -18,12 +71,16 @@ class StopwatchApp(App):
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
     ]
+    CSS_PATH = "stopwatch.css"
 
     def compose(self):
         """what widget is this app composed of?"""
         yield Header(show_clock=True)
         yield Footer()
-        yield Stopwatch()
+        with ScrollableContainer(id="stopwatches"):
+            yield Stopwatch()
+            yield Stopwatch()
+            yield Stopwatch()
 
 
 if __name__ == "__main__":
